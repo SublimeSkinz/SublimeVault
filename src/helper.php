98a533789a417eb481a1568ea5691d25b03cbe35
SublimeAmine <amine@sublimeskinz.com>
2018-02-14 13:37:32 +0100

    Create  helper

---
+++ src/helper.php
@@ -0,0 +1,40 @@
+<?php
+if (!function_exists('envS')) {
+
+    function envS($name)
+    {
+        $value = getenv($name);
+
+        //If we hev a value return it
+        if (strlen($value) > 0) {
+            return $value;
+        }
+
+        //Perepare session 
+        $request = curl_init();
+        $url = (string) getenv('VAULT_ADDR') . '/v1/secret/' . $name;
+        $token = (string) getenv('VAULT_TOKEN');
+        curl_setopt($request, CURLOPT_HTTPHEADER, ['X-Vault-Token: ' . $token]);
+        curl_setopt($request, CURLOPT_URL, $url);
+        curl_setopt($request, CURLOPT_RETURNTRANSFER, true);
+        
+        //get response
+        $response = curl_exec($request);
+        
+        //close session
+        curl_close($request);
+
+        //parse response
+        if ($response === false) {
+            return false;
+        }
+        
+        $r = json_decode($response, true);
+        
+        if (isset($r['data']['value'])) {
+            return (string) $r['data']['value'];
+        } 
+        
+        return false;
+    }
+}
