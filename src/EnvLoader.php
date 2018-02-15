89ef913cc5a978bf38715e774c8d4b32b275d017
SublimeDamien <damien@sublimeskinz.com>
2018-02-15 16:33:59 +0100

    Add a first draft for app role authentification

--- src/EnvLoader.php
+++ src/EnvLoader.php
@@ -2,36 +2,58 @@
 
 namespace SublimeSkinz\SublimeVault;
 
+use Aws\S3\S3Client;
 use GuzzleHttp\Client;
 
 class EnvLoader {
 
-    public function envS($name, $default = '') {
-
+    public function __construct() {
         $options['base_uri'] = getenv('VAULT_ADDR');
-        $options['headers']['X-Vault-Token'] = getenv('VAULT_TOKEN');
+        $options['headers']['X-Vault-Token'] = getenv('VAULT_AUTH_METHOD') == "iam" ? $this->getVaultTokenWithIAM() : $this->getVaultTokenWithAppRole();
+        $this->vault = new Client($options);
+    }
 
-        $vaultClient = new Client($options);
-        
+    public function env($name, $default) {
+        $val = array_key_exists($_ENV[$name . "_VAULT"]) ? $this->fetchSecretFromVault(getenv($name . "_VAULT")) : getenv($name);
+
+        return strlen($val) > 0 ? $val : $default;
+    }
+
+    private function fetchSecretFromVault($secret_path) {
         try {
-            $response = $vaultClient->request('GET', '/v1/secret/' . $name);
-           
+            $response = $this->vault->request('GET', '/v1/secret/' . $secret_path);
+
             if ($response->getStatusCode() != 200) {
-                return strlen($default) == 0 ? false : $default;
+                return ;
             }
 
             $r = json_decode($response->getBody()->getContents(), true);
-            
+
             if (isset($r['data']['value'])) {
                 return (string) $r['data']['value'];
             }
-
-            return strlen($default) == 0 ? false : $default;
         } catch (\Exception $e) {
-            
+            return ;
         }
-        
-        return strlen($default) == 0 ? false : $default;
+    }
+
+    private function fetchAppRoleCreds($bucket_name, $creds_path) {
+        $s3 = S3Client::factory();
+
+        return json_decode($s3->getObject(array(
+                                "Bucket" => $bucket_name,
+                                "Key"    => $creds_path
+                          ))["Body"],
+                          true);
+    }
+
+    private function getVaultTokenWithAppRole() {
+      $c = new Client();
+      return json_decode($c->request("POST", getenv("VAULT_ADDR") ."/auth/approle/login", $this->fetchAppRoleCreds(getenv("VAULT_BUCKET_NAME"), getenv("VAULT_CREDS_PATH"))), true)["auth"]["client_token"];
+    }
+
+    private function getVaultTokenWithIAM(){
+        return ;
     }
 
 }
